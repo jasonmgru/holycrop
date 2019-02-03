4e507b0320f47d074c323c8444fc667a0abd4aba
Jason Gruenhagen <jasonmgru@gmail.com>
2019-02-03 05:42:59 -0600

    added nice scrolling

--- authkeeper.js
+++ authkeeper.js
@@ -1,6 +1,14 @@
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-        
+        let docRef = firebase.firestore().collection("users").doc(user.uid);
+        docRef.get().then((doc) => {
+            if (doc.exists) {
+                let data = doc.data();
+                document.getElementById("profileName").innerHTML = data.firstName + " " + data.lastName;
+            } else {
+                alert("There was an error retrieving profile information.");
+            }
+        });
     } else {
         window.location = "login.html";
     }
